53ddf25a9b252ede54f6c76c34d503e83c1d6ac8
dtHvinh <dangthehienvinh662@gmail.com>
2024-10-17 12:48:35 +0700

    Add private messaging and system message toggling
    Enhanced Chat.razor to support private messaging and system message toggling. Added a checkbox to toggle system messages and differentiated whispers from global messages. Updated backend logic to handle these features, including enums for message type and perspective.
    
    Modified GlobalChatHub.cs to support sending private messages with the correct perspective for both sender and receiver.
    
    Updated IChatClient.cs interface to include ReceiveWhisperMessage method with perspective parameter.

--- SimpleBlazorChatApp/SimpleBlazorChatApp/Hubs/GlobalChatHub.cs
+++ SimpleBlazorChatApp/SimpleBlazorChatApp/Hubs/GlobalChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using static SimpleBlazorChatApp.Client.Pages.Chat;
 
 namespace SimpleBlazorChatApp.Hubs;
 
@@ -12,6 +13,15 @@ public class GlobalChatHub : Hub<IChatClient>
 
         return base.OnConnectedAsync();
     }
+
+    public Task SendMessage(string user, string target, string message)
+    {
+        Clients.Client(user).ReceiveWhisperMessage(user, message, Perspective.Sender);
+        Clients.Client(target).ReceiveWhisperMessage(target, message, Perspective.Receiver);
+
+        return base.OnConnectedAsync();
+    }
+
 
     public override Task OnConnectedAsync()
     {
--- SimpleBlazorChatApp/SimpleBlazorChatApp/Hubs/IChatClient.cs
+++ SimpleBlazorChatApp/SimpleBlazorChatApp/Hubs/IChatClient.cs
@@ -1,10 +1,11 @@
-﻿namespace SimpleBlazorChatApp.Hubs;
+﻿using static SimpleBlazorChatApp.Client.Pages.Chat;
+
+namespace SimpleBlazorChatApp.Hubs;
 
 public interface IChatClient
 {
     public Task ReceiveMessage(string user, string message);
-
+    public Task ReceiveWhisperMessage(string user, string message, Perspective perspective);
     public Task UserConnected(string user);
-
     public Task UserDisconnected(string user);
 }
